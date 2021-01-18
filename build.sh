ea4244059324fd24fc009ec44d1901cc05abef08
Chris McClelland <prophet3636@gmail.com>
2021-01-18 16:53:26 +0000

    Added googletests, build.sh, etc

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CONFIG=Debug
+if [ $# -ge 1 ]; then
+  CONFIG=$1
+fi
+
+# Maybe configure
+if [ ! -e build ]; then
+  git submodule update --init --recursive
+  mkdir build
+  cd build
+  cmake \
+    -DCMAKE_BUILD_TYPE=${CONFIG} \
+    -DBUILD_TESTING=1 \
+    -DCMAKE_VERBOSE_MAKEFILE=0 \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DCMAKE_INSTALL_RPATH=\$ORIGIN/../lib \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    ..
+  cd ..
+fi
+
+# Maybe build
+if [ $# -lt 2 ] || [ $2 != "-nobuild" ]; then
+  cmake --build build
+  echo 'You can install with "sudo cmake --build build --target install"'
+fi
